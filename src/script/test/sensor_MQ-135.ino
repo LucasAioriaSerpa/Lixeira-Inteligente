ed9b9ac47162508b074c2ca4f1a0d1d1c2f5bbec
Flavia Cristina Fagundes <flaviacrisfagundes@gmail.com>
2025-04-28 19:25:10 -0300

    Feat: create test sensor MQ-135

---
+++ src/script/test/sensor_MQ-135.ino
@@ -0,0 +1,26 @@
+
+#define MQ2pin 8 // porta 8 no arduino
+
+int sensorValue;  // variável que vai guardar o valor do sensor
+
+void setup() {
+	Serial.begin(9600); 
+	Serial.println("Aquecendo o sensor MQ2");
+	delay(200); // tempo de aquecimento (na vida real são alguns minutos para o sensor aquecer)
+}
+
+void loop() {
+	sensorValue = digitalRead(MQ2pin); // lê a porta 8 
+    // MQ2pin → Retorna 1 se não houver gás detectado | Retorna 0 se houver gás
+
+	Serial.print("Digital Output: ");
+	Serial.print(sensorValue);
+	
+	if (sensorValue) {
+		Serial.println(" | Nenhum gás detectado - ");
+	} else {
+		Serial.println(" | Gás Detectado!");
+	}
+	
+	delay(2000);
+}
