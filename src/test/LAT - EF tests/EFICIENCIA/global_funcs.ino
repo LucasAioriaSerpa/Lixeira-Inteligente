e783e3fed768277366ff99e951baf7e284dbc7f7
Nikolas Malinowski <nikis260606@gmail.com>
2025-06-19 19:49:37 -0300

    testes para sensor ultrassônico

--- src/test/LAT - EF tests/EFICIENCIA/global_funcs.ino
+++ src/test/LAT - EF tests/EFICIENCIA/global_funcs.ino
@@ -1,20 +1,45 @@
 #include <Arduino.h>
 #include <stdio.h>
+#include <HX711.h>
+
+// funçoes auxiliares
+long microssegundosParaCentimetros(long microSegundos);
+long leituraUnicaCM(int trigPin, int echoPin, int trigDelay, int timeout);
+long lerDistanciaMediaCM(int trigPin, int echoPin, int trigDelay, int numAmostras);
+void lerAmonia();
+void lerPesoHX711();
+
 
 /**
- * @brief Mede a eficiência de leitura do sensor HC-SR04 (leituras por segundo).
- * @param trigPin Pino trigger.
- * @param echoPin Pino echo.
- */
-void testarEficienciaHCSR04(int trigPin, int echoPin)
+* @brief Mede a eficiência de leitura do sensor HC-SR04 interno (leituras por segundo).
+*/
+void testarEficienciaHCSR04Interno()
 {
     static int leituras = 0;
     static unsigned long t0 = millis();
-    lerDistanciaMediaCM(trigPin, echoPin, 10, 5);
+    lerDistanciaMediaCM(TRIG_INTERNO_PIN, ECHO_INTERNO_PIN, 10, 5);
     leituras++;
     if (millis() - t0 > 1000)
     {
-        Serial.print("Leituras HC-SR04/s: ");
+        Serial.print("Leituras HC-SR04 Interno/s: ");
+        Serial.println(leituras);
+        leituras = 0;
+        t0 = millis();
+    }
+}
+
+ /**
+ * @brief Mede a eficiência de leitura do sensor HC-SR04 externo (leituras por segundo).
+ */
+void testarEficienciaHCSR04Externo()
+{
+    static int leituras = 0;
+    static unsigned long t0 = millis();
+    lerDistanciaMediaCM(TRIG_EXTERNO_PIN, ECHO_EXTERNO_PIN, 5, 5);
+    leituras++;
+    if (millis() - t0 > 1000)
+    {
+        Serial.print("Leituras HC-SR04 Externo/s: ");
         Serial.println(leituras);
         leituras = 0;
         t0 = millis();
